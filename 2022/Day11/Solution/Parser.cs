e496c8cf44852aed5357d7cbf2472dd7fdaddcaa
Dennis Meelker <dennis.meelker@infi.nl>
2022-12-11 08:58:25 +0100

    Day11

---
+++ 2022/Day11/Solution/Parser.cs
@@ -0,0 +1,66 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solution
+{
+    public class Monkey
+    {
+        public required Queue<long> Items { get; set; } = new();
+        public required Func<long, long> WorryFunction { get; set; } = (x) => x;
+        public required long ThrowDivisor { get; set; }
+        public required int TrueMonkeyId { get; set; }
+        public required int FalseMonkeyId { get; set; }
+        public long InspectCount { get; set; } = 0;
+
+        public int GetNextMonkeyId(long item)
+        {
+            return item % ThrowDivisor == 0 ? TrueMonkeyId : FalseMonkeyId;
+        }
+    }
+
+    public static class Parser
+    {
+        public static List<Monkey> ParseInput(string input)
+        {
+            return input.ToSections().Select(ParseSection).ToList();
+        }
+
+        private static Monkey ParseSection(string section)
+        {
+            var lines = section.ToLines();
+
+            return new()
+            {
+                Items = new(lines[1].StripPrefix("  Starting items: ").ParseLongArray()),
+                WorryFunction = ParseWorryFunction(lines[2].StripPrefix("  Operation: new = ")),
+                ThrowDivisor = lines[3].StripPrefix("  Test: divisible by ").ParseLong(),
+                TrueMonkeyId = lines[4].StripPrefix("    If true: throw to monkey ").ParseInt(),
+                FalseMonkeyId = lines[5].StripPrefix("    If false: throw to monkey ").ParseInt()
+            };
+        }
+
+        private static Func<long, long> ParseWorryFunction(string input)
+        {
+            if (input == "old * old")
+            {
+                return (x) => x * x;
+            }
+            else if (input.StartsWith("old + "))
+            {
+                var value = long.Parse(input.Substring("old + ".Length));
+                return (x) => x + value;
+            }
+            else if (input.StartsWith("old * "))
+            {
+                var value = long.Parse(input.Substring("old * ".Length));
+                return (x) => x * value;
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
---
+++ 2022/Day11/Solution/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Solution
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var part1 = Part1(ParseInput());
+            var part2 = Part2(ParseInput());
+
+            Console.WriteLine($"Part 1: {part1} Part 2: {part2}");
+
+            if (part1 != 58056 || part2 != 15048718170)
+            {
+                throw new Exception();
+            }
+        }
+
+        public static List<Monkey> ParseInput() => Parser.ParseInput(File.ReadAllText("input.txt"));
+
+        public static long Part1(List<Monkey> monkeys) => Solve(monkeys, 20, false);
+        public static long Part2(List<Monkey> monkeys) => Solve(monkeys, 10_000, true);
+
+        private static long Solve(List<Monkey> monkeys, int rounds, bool part2)
+        {
+            var greatestDenominator = monkeys.Select(m => m.ThrowDivisor).Aggregate((x, y) => x * y);
+
+            for (var i = 0; i < rounds; i++)
+            {
+                foreach (var monkey in monkeys)
+                {
+                    while (monkey.Items.TryDequeue(out var item))
+                    {
+                        monkey.InspectCount++;
+                        item = monkey.WorryFunction(item);
+
+                        if (part2)
+                        {
+                            item = item % greatestDenominator;
+                        }
+                        else
+                        {
+                            item = (long)Math.Floor(item / 3.0);
+                        }
+
+                        monkeys[monkey.GetNextMonkeyId(item)].Items.Enqueue(item);
+                    }
+                }
+            }
+
+            return monkeys
+                .Select(m => m.InspectCount)
+                .OrderByDescending(v => v)
+                .Take(2)
+                .Aggregate((x, y) => x * y);
+        }
+    }
+}
