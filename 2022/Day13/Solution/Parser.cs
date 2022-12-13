d86e5dbf73ac106b6bc1e2823cb281fd54aa6715
Dennis Meelker <dennis.meelker@infi.nl>
2022-12-13 08:04:27 +0100

    Day13

---
+++ 2022/Day13/Solution/Parser.cs
@@ -0,0 +1,154 @@
+﻿using Shared;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Solution
+{
+    [DebuggerDisplay("{Left} <=> {Right}")]
+    public class ValuePair
+    {
+        public Value Left { get; set; }
+        public Value Right { get; set; }
+
+        public ValuePair(Value left, Value right)
+        {
+            Left = left;
+            Right = right;
+        }
+
+        public override string ToString()
+        {
+            return $"{Left}\n{Right}";
+        }
+    }
+
+    public abstract class Value
+    { }
+
+    [DebuggerDisplay("{Value}")]
+    public class NumericValue : Value
+    {
+        public int Value { get; set; }
+        public NumericValue(int value) => Value = value;
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+
+    [DebuggerDisplay("{Values}")]
+    public class ListValue : Value
+    {
+        public List<Value> Values { get; set; } = new();
+
+        public ListValue() { }
+
+        public ListValue(params Value[] values)
+        {
+            Values = values.ToList();
+        }
+
+        public override string ToString()
+        {
+            return "[" + string.Join(",", Values.Select(v => v.ToString())) + "]";
+        }
+    }
+
+    public static class Parser
+    {
+        public static List<ValuePair> ParseInput(string input)
+        {
+            return input.ToSections().Select(ParsePairs).ToList();
+        }
+
+        public static ValuePair ParsePairs(string input)
+        {
+            var lines = input.ToLines();
+            return new(ParseValue(lines[0]), ParseValue(lines[1]));
+        }
+
+        public static Value ParseValue(string input)
+        {
+            ListValue root = new();
+            var values = new Stack<ListValue>();
+            values.Push(root);
+
+            input = input.Substring(1, input.Length - 2); // Strip the outer most brackets
+            var reader = new CharacterReader(input);
+
+            while (reader.CanRead)
+            {
+                var nextChar = reader.Peek();
+                if (nextChar == '[')
+                {
+                    var newValue = new ListValue();
+                    values.Peek().Values.Add(newValue);
+                    values.Push(newValue);
+                    reader.Read();
+                }
+                else if (nextChar == ']')
+                {
+                    values.Pop();
+                    reader.Read();
+                }
+                else if (char.IsNumber(nextChar))
+                {
+                    var value = reader.ReadInt();
+                    values.Peek().Values.Add(new NumericValue(value));
+                }
+                else if (nextChar == ',')
+                {
+                    reader.Read();
+                }
+            }
+
+            return root;
+        }
+    }
+
+    public class CharacterReader
+    {
+        private string _source;
+        private int _index = 0;
+
+
+        public CharacterReader(string input)
+        {
+            _source = input;
+        }
+
+        public bool CanRead => _index < _source.Length;
+        public char Read()
+        {
+            var chr = _source[_index];
+            _index++;
+            return chr;
+        }
+
+        public char Peek()
+        {
+            return _source[_index];
+        }
+
+        public int ReadInt()
+        {
+            var str = new StringBuilder();
+            while (CanRead)
+            {
+                if (char.IsNumber(Peek()))
+                {
+                    str.Append(Read());
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return int.Parse(str.ToString());
+        }
+    }
+}
---
+++ 2022/Day13/Solution/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Solution
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Parser.ParseInput(File.ReadAllText("input.txt"));
+
+            var part1 = Part1(input);
+            var part2 = Part2(input);
+
+            Console.WriteLine($"Part 1: {part1} Part 2: {part2}");
+
+            if (part1 != 5623 || part2 != 20570)
+            {
+                throw new Exception();
+            }
+        }
+
+        public static int Part1(List<ValuePair> input)
+        {
+            var comparer = new ValueComparer();
+
+            return input
+                .Select((value, index) => comparer.Compare(value.Left, value.Right) == -1 ? index + 1 : 0)
+                .Sum();
+        }
+
+        public static int Part2(List<ValuePair> input)
+        {
+            var dividerPackets = new Value[] {
+                new ListValue(new ListValue(new NumericValue(2))),
+                new ListValue(new ListValue(new NumericValue(6))),
+            };
+
+            var packets = input.SelectMany(p => new[] { p.Left, p.Right })
+                .Concat(dividerPackets)
+                .OrderBy(packet => packet, new ValueComparer())
+                .ToList();
+
+            return (packets.IndexOf(dividerPackets[0]) + 1) * (packets.IndexOf(dividerPackets[1]) + 1);
+        }
+    }
+
+    public class ValueComparer : IComparer<Value>
+    {
+        public int Compare(Value left, Value right)
+        {
+            if (left is NumericValue leftN && right is NumericValue rightN)
+            {
+                return CompareNumericValues(leftN, rightN);
+            }
+            else
+            {
+                return CompareListValues(ToList(left), ToList(right));
+            }
+        }
+
+        public ListValue ToList(Value value)
+        {
+            if (value is ListValue listValue)
+            {
+                return listValue;
+            }
+            else
+            {
+                return new ListValue(value);
+            }
+        }
+
+        public int CompareListValues(ListValue left, ListValue right)
+        {
+            var count = Math.Max(left.Values.Count, right.Values.Count);
+            for (var i = 0; i < count; i++)
+            {
+                if (i >= left.Values.Count)
+                {
+                    return -1;
+                }
+
+                if (i >= right.Values.Count)
+                {
+                    return 1;
+                }
+
+                var result = Compare(left.Values[i], right.Values[i]);
+                if (result != 0)
+                {
+                    return result;
+                }
+            }
+
+            return 0;
+        }
+
+        public int CompareNumericValues(NumericValue left, NumericValue right)
+        {
+            return left.Value.CompareTo(right.Value);
+        }
+    }
+}
