0428800dcf2b715b6f57c5b16cb2b9d2d7a37183
Dennis Meelker <dmeelker@gmail.com>
2023-12-08 11:56:40 +0100

    Day 8

---
+++ 2023/Day08/Solution/Parser.cs
@@ -0,0 +1,31 @@
+﻿using Shared;
+using System.Linq;
+
+namespace Solution;
+
+public static class Parser
+{
+    public static Input ParseInput(string input)
+    {
+        var sections = input.ToSections();
+        var directions = sections[0];
+
+        var nodes = sections[1]
+            .ToLines()
+            .Select(ParseNode)
+            .ToArray();
+
+        return new Input(directions, nodes);
+    }
+
+    private static Node ParseNode(string input)
+    {
+        var parts = input.Replace("(", "")
+            .Replace(")", "")
+            .Replace("=", " ")
+            .Replace(",", " ")
+            .Split(' ', System.StringSplitOptions.RemoveEmptyEntries);
+
+        return new Node(parts[0], parts[1], parts[2]);
+    }
+}
---
+++ 2023/Day08/Solution/Program.cs
@@ -0,0 +1,63 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Solution;
+
+public record Input(string Directions, Node[] Nodes);
+public record Node(string Name, string Left, string Right);
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var input = Parser.ParseInput(File.ReadAllText("input.txt"));
+        var part1 = Part1(input);
+        var part2 = Part2(input);
+
+        Console.WriteLine($"Part 1: {part1} Part 2: {part2}");
+
+        if (part1 != 16043 || part2 != 15726453850399L)
+        {
+            throw new Exception();
+        }
+    }
+
+    public static int Part1(Input input)
+    {
+        var nodeLookup = input.Nodes.ToDictionary(n => n.Name);
+
+        return FindPathLength("AAA", name => name == "ZZZ", nodeLookup, input.Directions);
+    }
+
+    public static long Part2(Input input)
+    {
+        var nodeLookup = input.Nodes.ToDictionary(n => n.Name);
+
+        return input.Nodes.Where(n => n.Name.EndsWith("A"))
+            .Select(node => (long)FindPathLength(node.Name, name => name.EndsWith("Z"), nodeLookup, input.Directions))
+            .Aggregate(Arithmetic.LCM);
+    }
+
+    private static int FindPathLength(string startName, Func<string, bool> endNameFilter, Dictionary<string, Node> nodeLookup, string directions)
+    {
+        var currentNodeName = startName;
+        var directionIndex = 0;
+        var step = 0;
+
+        while (!endNameFilter(currentNodeName))
+        {
+            var node = nodeLookup[currentNodeName];
+            var direction = directions[directionIndex];
+
+            currentNodeName = direction == 'L' ? node.Left : node.Right;
+
+            directionIndex = (directionIndex + 1) % directions.Length;
+            step++;
+        }
+
+        return step;
+    }
+}
