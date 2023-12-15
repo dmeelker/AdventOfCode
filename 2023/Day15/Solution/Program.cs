1cfb5c1bf714ea98e0cb3896dd32f7fa074be471
Dennis Meelker <dennis@infi.nl>
2023-12-15 08:35:42 +0100

    Day 15

---
+++ 2023/Day15/Solution/Program.cs
@@ -0,0 +1,90 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Solution;
+
+record Instruction(string Label, char Operation, int? Argument);
+record Lens(string Label, int FocalLength);
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var input = File.ReadAllText("input.txt");
+        var part1 = Part1(input);
+        var part2 = Part2(input);
+
+        Console.WriteLine($"Part 1: {part1} Part 2: {part2}");
+
+        if (part1 != 512283 || part2 != 215827)
+        {
+            throw new Exception();
+        }
+    }
+
+    public static int Part1(string input)
+    {
+        return input.Split(',').Sum(Hash);
+    }
+
+    private static int Hash(string input)
+    {
+        return input.ToCharArray()
+            .Select(c => (int)c)
+            .Aggregate(0, (v1, v2) => ((v1 + v2) * 17) % 256);
+    }
+
+    public static int Part2(string input)
+    {
+        var instructions = input.Split(',').Select(ParseInstruction).ToArray();
+
+        var boxes = Enumerable.Range(0, 256)
+            .Select(i => new List<Lens>())
+            .ToArray();
+
+        foreach (var instruction in instructions)
+        {
+            var box = boxes[Hash(instruction.Label)];
+
+            if (instruction.Operation == '=')
+            {
+                var lens = new Lens(instruction.Label, instruction.Argument!.Value);
+
+                var existingLens = box.Find(l => l.Label == instruction.Label);
+                box!.ReplaceOrAdd(existingLens, lens);
+            }
+            else
+            {
+                box.RemoveAll(l => l.Label == instruction.Label);
+            }
+        }
+
+        return boxes.Select((box, boxIndex) =>
+            box.Select((lens, lensIndex) => LensScore(boxIndex, lensIndex, lens.FocalLength)).Sum()
+        ).Sum();
+    }
+
+    private static int LensScore(int boxIndex, int lensIndex, int focalLength)
+    {
+        return (1 + boxIndex) * (lensIndex + 1) * focalLength;
+    }
+
+    private static Instruction ParseInstruction(string input)
+    {
+        if (input.IndexOf('=') != -1)
+        {
+            var parts = input.Split('=');
+            var label = parts[0];
+            var focalLength = int.Parse(parts[1]);
+            return new(label, '=', focalLength);
+        }
+        else
+        {
+            var label = input.Substring(0, input.Length - 1);
+            return new(label, '-', null);
+        }
+    }
+}
--- 2023/Shared/ListExtensions.cs
+++ 2023/Shared/ListExtensions.cs
@@ -8,5 +8,26 @@
             list.RemoveAt(list.Count - 1);
             return last;
         }
+
+        public static bool Replace<T>(this List<T> list, T oldItem, T newItem)
+        {
+            var index = list.IndexOf(oldItem);
+            if (index == -1)
+            {
+                return false;
+            }
+
+            list.RemoveAt(index);
+            list.Insert(index, newItem);
+            return true;
+        }
+
+        public static void ReplaceOrAdd<T>(this List<T> list, T oldItem, T newItem)
+        {
+            if (!list.Replace(oldItem, newItem))
+            {
+                list.Add(newItem);
+            }
+        }
     }
 }
